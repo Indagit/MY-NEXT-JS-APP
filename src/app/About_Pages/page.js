24f0e714901a710fd896f8cc1ebb074620e20e8f
INDAWOLF <Indawolf@gmail.com>
2026-03-19 10:11:37 +1100

    Removing about buttons

--- src/app/About_Pages/page.js
+++ src/app/About_Pages/page.js
@@ -6,9 +6,6 @@ function page() {
     <div>
       <h1>This is the About Page</h1>
     </div>
-    <Link href="/Home_Page"> 
-    <button style={{ color: 'purple'}}> Go to Home Page </button>
-    </Link>
     <form>
       <input type="text" style={{color : 'green'}}></input>
     </form>
