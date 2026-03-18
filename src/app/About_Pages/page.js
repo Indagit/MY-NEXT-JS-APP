3fbf920eacae28052f31d799f4b79b536ea03a01
INDAWOLF <Indawolf@gmail.com>
2026-03-19 10:13:34 +1100

    testing forms

--- src/app/About_Pages/page.js
+++ src/app/About_Pages/page.js
@@ -6,7 +6,7 @@ function page() {
     <div>
       <h1>This is the About Page</h1>
     </div>
-    <form>
+    <form name="contact" netlify>
       <input type="text" style={{color : 'green'}}></input>
     </form>
     </>
