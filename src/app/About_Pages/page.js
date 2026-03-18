e990c0b1b10719bb1c1b6a963b68b283f4d1c96a
Indawolf <indawolf@gmail.com>
2026-03-18 21:20:17 +1100

    Adding Input Text box

--- src/app/About_Pages/page.js
+++ src/app/About_Pages/page.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 function page() {
   return (
+    <>
     <div>
       <h1>This is the About Page</h1>
     </div>
+
+    <form>
+      <input type="text" style={{color : 'green'}}></input>
+    </form>
+    </>
   )
 }
 
